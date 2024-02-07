328964ecf719f8d587783a69f28b18428166bf92
Jay Hannah <jay.hannah@iinteractive.com>
2024-02-07 12:04:00 -0600

    2016 site grabbed from Internet Archive Wayback Machine
    https://web.archive.org/web/20160313120519/http://opennebraska.io/

---
+++ padhang.js
@@ -0,0 +1,41 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+// custom theme function
+jQuery(document).ready(function($){
+	var url = $('body.custom-background').css('background-image');
+	url = url.replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
+	
+	// init the Backstretch
+	$.backstretch( url );
+});
+
+}
+/*
+     FILE ARCHIVED ON 08:33:15 Feb 22, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 17:58:30 Feb 07, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots: 0.069
+  exclusion.robots.policy: 0.058
+  cdx.remote: 0.105
+  esindex: 0.01
+  LoadShardBlock: 1427.389 (6)
+  PetaboxLoader3.datanode: 1302.527 (7)
+  load_resource: 354.708
+  PetaboxLoader3.resolve: 219.068
+*/
---
+++ skip-link-focus-fix.js
@@ -0,0 +1,53 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+( function() {
+	var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
+	    is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
+	    is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
+
+	if ( ( is_webkit || is_opera || is_ie ) && 'undefined' !== typeof( document.getElementById ) ) {
+		var eventMethod = ( window.addEventListener ) ? 'addEventListener' : 'attachEvent';
+		window[ eventMethod ]( 'hashchange', function() {
+			var element = document.getElementById( location.hash.substring( 1 ) );
+
+			if ( element ) {
+				if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) )
+					element.tabIndex = -1;
+
+				element.focus();
+			}
+		}, false );
+	}
+})();
+
+
+}
+/*
+     FILE ARCHIVED ON 01:53:52 Apr 04, 2016 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 17:58:23 Feb 07, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  exclusion.robots: 0.148
+  exclusion.robots.policy: 0.136
+  cdx.remote: 0.151
+  esindex: 0.013
+  LoadShardBlock: 339.534 (6)
+  PetaboxLoader3.datanode: 510.728 (8)
+  load_resource: 429.292 (2)
+  PetaboxLoader3.resolve: 180.443 (2)
+*/
